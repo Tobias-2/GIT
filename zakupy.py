e092ebc92fedbf0886622140e5a0966dd2b9d835
Tobiasz Błach <tobiaszblach23@gmail.com>
2024-09-04 11:31:03 +0200

    End

--- zakupy.py
+++ zakupy.py
@@ -8,4 +8,9 @@ for sklep in zakupy:
     sentence = ("idę do {sklep} i kupuję tam {zakupy[sklep]}")
     print(sentence.title())
 
+w = (len(sklepy["warzywniak"]))
+p = (len(sklepy["piekarnia"]))
+t = w + p
+
+print(f"W sumie kupuję {t} produktów")
 
