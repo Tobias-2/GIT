072f380095ae7a331c691ad0fe5402b0f6d92076
Tobiasz Błach <tobiaszblach23@gmail.com>
2024-09-09 12:14:40 +0200

    Zadanie palindromy

---
+++ palindromy.py
@@ -0,0 +1,14 @@
+def palindromy(front):
+
+    back = front [::-1]
+
+    if front == back:
+        print("true")
+
+    else:
+        print("false") 
+
+palindromy("ale")
+palindromy("ala") 
+         
+
